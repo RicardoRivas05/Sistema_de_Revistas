14bcc5b1b35b6b21f4c98a6c05d6283d9b94ce89
RicardoRivas <you@example.com>
2024-01-31 23:34:30 -0600

    Login, Menu y primeros Avances

---
+++ Sistema de revistas/forms/frmMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_revistas.forms
+{
+    public partial class frmMenu : Form
+    {
+        private frmLogin loginForm;
+        public frmMenu(frmLogin loginForm)
+        {
+            InitializeComponent();
+            this.loginForm = loginForm;
+        }
+        
+
+        private void frmMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            loginForm.Show();
+        }
+    }
+}
