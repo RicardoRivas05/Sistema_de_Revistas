87b6f95c57da67bcef430e3515486129da7ae978
juarez02 <158406922+juarez02@users.noreply.github.com>
2024-02-01 03:28:29 -0600

    Add files via upload
    pantalla de recuperar usuario, editores y entrevista

---
+++ Sistema_de_Revistas-main/Sistema de revistas/forms/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_revistas
+{
+    public partial class frmLogin : Form
+    {
+        Form menu;
+        Form crearUser;
+        Form recuperar;
+        public frmLogin()
+        {
+            InitializeComponent();
+            menu = new forms.frmMenu(this);
+            crearUser = new forms.CrearCuenta(this);
+            recuperar = new forms.Recuperarusuario(this);
+        }
+
+        private void btnIniciarS_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            menu.Show();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            crearUser.Show();
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            recuperar.Show();
+        }
+    }
+}
---
+++ Sistema_de_Revistas-main/Sistema de revistas/forms/Recuperarusuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_revistas.forms
+{
+    public partial class Recuperarusuario : Form
+    {
+        private frmLogin loginForm;
+        public Recuperarusuario(frmLogin loginForm)
+        {
+            InitializeComponent();
+            this.loginForm = loginForm;
+        }
+
+        private void txtCancelar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            loginForm.Show();
+        }
+    }
+}
